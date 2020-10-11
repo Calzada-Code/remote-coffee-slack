5c9d62fa41afbf75972460f616cf210591049d87
Vitalii <ananichev.v.v@gmail.com>
2020-10-11 17:55:02 +0300

    Fix tests

--- test/remote_coffee_slack_test.rb
+++ test/remote_coffee_slack_test.rb
@@ -3,12 +3,12 @@ require "test_helper"
 class RemoteCoffeeSlackTest < Minitest::Test
   def test_default_configuration
     config = RemoteCoffeeSlack::Configuration.new
-    assert_equal config.slack_channel, '#general'
+    assert_equal '#general', config.slack_channel
   end
 
   def test_configuration
     config = RemoteCoffeeSlack::Configuration.new
     config.slack_channel = '#new_channel'
-    assert_equal config.slack_channel, '#new_channel'
+    assert_equal '#new_channel', config.slack_channel
   end
 end
